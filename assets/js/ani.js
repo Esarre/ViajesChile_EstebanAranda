84f3cb8f58150456455c7443c018860c701e2ea7
Esarre <e.aranda.re@gmail.com>
2024-03-27 19:51:17 -0300

    ajustes de cards, ajuste del hide (head), ajustes boton Enviar

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -1,5 +1,8 @@
 $(function(){
 
-
+    $(".msgalert").click(function(){
+        $(this).addClass("ftrbtngreen");
+        alert("Tu mensaje se ha enviado satisfactoriamente! Pronto nos comunicaremos contigo para asesorarte con todas tus dudas.\n\nFeliz Día !");
+    })
     
 })
