39d109f7d5bc370b4ab731a4bd23aac12dce0580
Esarre <e.aranda.re@gmail.com>
2024-03-27 22:11:39 -0300

    cambio color de bkgr navbar (scroll), ajustes menores, comentarios css y js

--- assets/js/ani.js
+++ assets/js/ani.js
@@ -1,3 +1,30 @@
+    /** JavaScript puro */
+
+/*
+Agrega el cambio en el color de background cuando el usuario se
+desplaza hacia abajo por la página, logrando que el texto del Navbar
+no se funda con los fondos claros
+*/  
+
+window.addEventListener('scroll', function() {
+    const navbar = document.querySelector('.navbar');
+    if (window.scrollY > 1300) {
+        navbar.classList.add('scrolled');
+    } else {
+        navbar.classList.remove('scrolled');
+    }
+});
+
+
+
+    /** jQuery */
+
+/** 
+Al hacer click, añade un mensaje de alerta indicándole al usuario
+que su mensaje ha sido enviado satisfactoriamente. Además, cambia el
+color del botón a verde luego de la acción. 
+ */
+
 $(function(){
 
     $(".msgalert").click(function(){
